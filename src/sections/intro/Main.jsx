4f2488dcc960908e0120de900b7c7f50916aa630
DESKTOP-TUAT6AH\DM51 <lockhun@doublemedia.co.kr>
2023-02-03 14:40:31 +0900

    hoitfix

--- src/sections/intro/Main.jsx
+++ src/sections/intro/Main.jsx
@@ -1,13 +1,8 @@
 // @mui
-import { Alert, Tooltip, Stack, Typography, Link, Box } from '@mui/material';
-import LanguagePopover from 'src/layouts/dashboard/header/LanguagePopover';
 import MainLayout from 'src/layouts/main/MainLayout';
 // auth
 // layouts
-import LoginLayout from '../../layouts/login';
 //
-import AuthLoginForm from '../auth/AuthLoginForm';
-import AuthWithSocial from '../auth/AuthWithSocial';
 
 // ----------------------------------------------------------------------
 
